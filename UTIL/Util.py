03f9306b8103adb5d992a6db32f06457921ed9f6
vaishalibokadiya <vaishali.bokadiya@gmail.com>
2024-01-04 14:18:21 +0530

    added env

--- UTIL/Util.py
+++ UTIL/Util.py
@@ -1,20 +1,32 @@
 import mysql.connector as sql
 import sys
 sys.path.insert(0,'D:/Vaishali Bokadiya/Python/CarConnect/EXCEPTION')
+
 from DatabaseException import DatabaseConnectionException
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Class for connecting to the database
 class DBPropertyUtil:
     @staticmethod
     def get_connection_string():
         try:
-            mydb = sql.connect(host='localhost',user='root',password='Vaishali@1234',database='CarConnect')
+            host=os.getenv("HOST")
+            user=os.getenv("USER")
+            password=os.getenv("PASSWORD")
+            database=os.getenv("DATABASE")
+
+            mydb = sql.connect(host=host,user=user,password=password,database=database)
         except DatabaseConnectionException as e:
             print(e.message)
         else:
             # print('Successfully connected to the database.')
             return mydb
     
+
 # Class for getting cursor
 class DBConnUtil:
     @staticmethod
